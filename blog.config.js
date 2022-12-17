2ba50caa42d51c4b25dd3b2b2db74b0ab54dd7c9
nicocanta20 <nicocanta20@gmail.com>
2022-12-17 11:14:38 -0300

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'Auralizeinfo@gmail.com',
   link: 'https://auralize-beryl.vercel.app',
   description: 'AI-Powered Podcast Curation',
-  lang: ['en-US','es-ES'], // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
